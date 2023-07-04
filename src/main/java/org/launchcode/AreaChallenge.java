2b2669d866b04676122df81203dfa2afe43113ed
Delaney <delaney.rosa1@gmail.com>
2023-07-03 21:13:06 -0500

    first commit--completed coding challeng for 7/3

---
+++ src/main/java/org/launchcode/AreaChallenge.java
@@ -0,0 +1,8 @@
+package org.launchcode;
+
+public class AreaChallenge {
+    public static int findArea(int base, int height){
+        int triArea= (base*height)/2;
+        return triArea;
+    }
+}
---
+++ src/main/java/org/launchcode/Main.java
@@ -0,0 +1,7 @@
+package org.launchcode;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(AreaChallenge.findArea(3,2));
+    }
+}
