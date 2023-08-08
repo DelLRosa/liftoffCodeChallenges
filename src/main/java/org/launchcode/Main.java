fc502fa5b343a03f220ed8e9812186184314de15
Delaney Green <delaney.rosa1@gmail.com>
2023-08-08 12:54:54 -0500

    Completed coding challenge for 8/7

--- src/main/java/org/launchcode/Main.java
+++ src/main/java/org/launchcode/Main.java
@@ -1,14 +1,24 @@
 package org.launchcode;
 
+import org.launchcode.ageCompare.Person;
+
 public class Main {
     public static void main(String[] args) {
 //
 //        System.out.println(AreaChallenge.findArea(3,2));
 //        System.out.println(StringReverse.FirstReverse("Hello"));
 //        System.out.println(LongestString.findLongestWord("it@$% is ME!!!!!!!, asdfgh#$%^&"));
-        System.out.println(AddNumber.addNumber(4));
-        System.out.println(AddNumber.addNumber(13));
-        System.out.println(AddNumber.addNumber(600));
+//        System.out.println(AddNumber.addNumber(4));
+//        System.out.println(AddNumber.addNumber(13));
+//        System.out.println(AddNumber.addNumber(600));
+        Person p1 = new Person("Joey", 27);
+        Person p2 = new Person("Bobby", 27);
+        Person p3 = new Person("Mandy", 55);
+        Person p4 = new Person("Sue", 15);
+        System.out.println(p1.compareAge(p2));
+        System.out.println(p1.compareAge(p3));
+        System.out.println(p1.compareAge(p4));
+        System.out.println(p2.compareAge(p4));
     }
 
 }
---
+++ src/main/java/org/launchcode/ageCompare/Person.java
@@ -0,0 +1,37 @@
+package org.launchcode.ageCompare;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String compareAge(Person comparePerson) {
+        if (this.getAge()>comparePerson.getAge()){
+            return comparePerson.getName() + " is younger than me." ;
+        } else if (this.getAge()< comparePerson.getAge()){
+            return comparePerson.getName() + " is older than me.";
+        } else {
+            return comparePerson.getName() + " is the same age as me.";
+        }
+    }
+}
